2ca02cf127774dda79212ced64a187fbb53e3901
Sudhir Mishra <admin@innit.social>
2026-06-16 17:21:07 +0100

    Disable generated security user

--- src/main/java/com/innitsocial/abcdish/common/config/SecurityBeanConfig.java
+++ src/main/java/com/innitsocial/abcdish/common/config/SecurityBeanConfig.java
@@ -2,6 +2,8 @@ package com.innitsocial.abcdish.common.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -11,5 +13,12 @@ public class SecurityBeanConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return username -> {
+            throw new UsernameNotFoundException("Password login is disabled");
+        };
     }
 }
